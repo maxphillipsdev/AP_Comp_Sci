debc37f77829f8ed2d447dfa579c7da101fa05d1
chilisource <chilisource@users.noreply.github.com>
2017-08-28 09:11:18 +0200

    Did excercises and project

---
+++ Chapter 3/src/RectanglePerimeter.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author 2020_maxwell_phillips
+ *
+ */
+public class RectanglePerimeter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int width = 4;
+		int length = 13;
+		int perimeter = 2*width + 2*length;
+		System.out.print("The perimeter of a rectangle with width 4 & length 13 is: ");
+		System.out.print(perimeter);
+	}
+
+}
